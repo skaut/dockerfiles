bba7eb8b4b7223423bbee8429752bdb0ddc544fa
František Hána <sinacek@gmail.com>
2022-03-13 13:16:30 +0100

    cleanup

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 
-docker build -t fmasa/lebeda:8.1 lebeda/8.1 --no-cache
-docker build -t fmasa/lebeda:8.1-ci lebeda/8.1-ci --no-cache
+docker build -t skaut/lebeda:8.1 lebeda/8.1 --no-cache
+docker build -t skaut/lebeda:8.1-ci lebeda/8.1-ci --no-cache
 
-docker build -t fmasa/lebeda:8.0 lebeda/8.0 --no-cache
-docker build -t fmasa/lebeda:8.0-ci lebeda/8.0-ci --no-cache
+docker build -t skaut/lebeda:8.0 lebeda/8.0 --no-cache
+docker build -t skaut/lebeda:8.0-ci lebeda/8.0-ci --no-cache
 
-docker build -t fmasa/lebeda:7.4 lebeda/7.4 --no-cache
-docker build -t fmasa/lebeda:7.4-ci lebeda/7.4-ci --no-cache
+docker build -t skaut/lebeda:7.4 lebeda/7.4 --no-cache
+docker build -t skaut/lebeda:7.4-ci lebeda/7.4-ci --no-cache
 
-docker build -t fmasa/lebeda:7.3 lebeda/7.3 --no-cache
-docker build -t fmasa/lebeda:7.3-ci lebeda/7.3-ci --no-cache
 
-docker build -t fmasa/lebeda:7.1 lebeda/7.1 --no-cache
-docker build -t fmasa/lebeda:7.1-ci lebeda/7.1-ci --no-cache
-
-docker build -t fmasa/firefox firefox --no-cache
-
-docker push fmasa/lebeda
-docker push fmasa/firefox
+docker push skaut/lebeda:8.1
+docker push skaut/lebeda:8.1-ci
+docker push skaut/lebeda:8.0
+docker push skaut/lebeda:8.0-ci
+docker push skaut/lebeda:7.4
+docker push skaut/lebeda:7.4-ci
